e7096c46a510cd5164955e3d330349e5d9d0c9ce
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-01-09 20:57:47 +0100

    init

---
+++ src/main/java/UtilityCode.java
@@ -0,0 +1,66 @@
+import org.javacord.api.event.message.MessageEvent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class UtilityCode {
+    public static ArrayList<String> getPokemonList(String regexReady){
+        System.out.println("input: " + regexReady);
+        regexReady = regexReady.replaceAll(".*pok\\u00E9mon is ", "").replaceAll("\\\\", "").replaceAll("\\u00E9", "é")
+                .replaceAll("_", ".").replaceAll("\\u2642", "")
+                .replaceAll("\\uFE0F","").replaceAll(" ", "");
+        System.out.println("searching: " + regexReady);
+        ArrayList<String> list = new ArrayList<>();
+
+        File csvFile = new File("./files/pokemonList.txt");
+        try {
+            Scanner fileReader = new Scanner(csvFile);
+            while (fileReader.hasNextLine()){
+                String next = fileReader.nextLine();
+                if (next.toLowerCase().matches(regexReady.toLowerCase())){
+                    list.add(next);
+                }
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public static void returnMessageReady(ArrayList<String> list, MessageEvent event){
+        ArrayList<String> found = new ArrayList<>();
+        String msg = "`I have found the following: `";
+        System.out.println("Found:");
+
+        if (list.size() > 15) {
+            msg = "`looks like something went wrong and too many results came back, please try again with a different hint.`";
+            event.getChannel().sendMessage(msg);
+            System.out.println("Too many results");
+        }
+        else if (list.size()==0){
+            msg = "`I haven't found anything.`";
+            event.getChannel().sendMessage(msg);
+            System.out.println("Nothing");
+        }
+        else {
+            event.getChannel().sendMessage(msg);
+            for (String s: list) {
+                event.getChannel().sendMessage("`" + s + "`");
+                System.out.println(s);
+            }
+        }
+
+    }
+
+   /* public static void transform(String woord,MessageEvent event){
+        woord = woord.replaceAll("\\\\", "").replaceAll("\\u00E9", "é")
+                .replaceAll("_", ".").replaceAll("\\u2642", "")
+                .replaceAll("\\uFE0F","");
+        System.out.println(woord);
+        event.getChannel().sendMessage("I see: " + woord);
+
+    }*/
+}
---
+++ src/main/java/bot.java
@@ -0,0 +1,60 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.ActivityType;
+import org.javacord.api.entity.user.UserStatus;
+
+import java.util.Locale;
+
+public class bot {
+
+    public static void main(String[] args) {
+        String spawnChannel = "928371104626659328";
+        long pokeTwoID = 716390085896962058L;
+        long botID = 713021503221792799L;
+        final String prefix = "tb!";
+
+
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("NzEzMDIxNTAzMjIxNzkyNzk5.XsaDBQ._NgTXsMB3sLFSxG9KxdDnsJAKH8")
+                .login().join();
+
+
+        api.updateStatus(UserStatus.DO_NOT_DISTURB);
+        api.updateActivity(ActivityType.PLAYING," Poketwo ");
+        api.addMessageCreateListener(event -> {
+            String msg = event.getMessageContent();
+
+            if (event.getMessageContent().toLowerCase().startsWith(prefix)){
+                System.out.println("Message with prefix");
+                String[] att = msg.split(" ");
+                event.getChannel().sendMessage("");
+                if (att.length>1){
+                    if (att[1].equalsIgnoreCase("hint")){
+                        System.out.println("hint");
+                        if (att.length>2 && !(att[2].equalsIgnoreCase(" "))){
+                            System.out.println(att[2]);
+                            UtilityCode.returnMessageReady(UtilityCode.getPokemonList(att[2]
+                                    ), event);
+
+                        } else {
+                            event.getChannel().sendMessage("Usage: " + prefix + " <hint from poketwo bot>\nWatch out for spaces!");
+                        }
+                    }
+                }
+            }
+
+            if (event.getMessage().getAuthor().getId() == pokeTwoID) {
+                System.out.println("Poketwo send message in: " +"Server: "+ event.getServer() + " txt:" + event.getChannel());
+                if (event.getMessageContent().matches(".*pok\\u00E9mon is.*\\.")) {
+                    System.out.println("Got hint in: " + event.getChannel());
+                    String tempName = event.getMessageContent().replaceAll("\\.", "");
+                    System.out.println("tempname: " + tempName);
+                    UtilityCode.returnMessageReady(UtilityCode.getPokemonList(tempName), event);
+
+                }
+            }
+
+        });
+
+    }
+}
